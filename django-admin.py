624165290da092d64b5d24eeeda5a6998699d06b
vimlili <vimlili@webank.com>
2015-09-06 16:52:59 +0800

    初始化项目

---
+++ django-admin.py
@@ -0,0 +1,4 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+# EASY-INSTALL-SCRIPT: 'Django==1.7.1','django-admin.py'
+__requires__ = 'Django==1.7.1'
+__import__('pkg_resources').run_script('Django==1.7.1', 'django-admin.py')
